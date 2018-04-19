4b055f382cbfe8d1ac56d83085e36156c2026b76
Unknown <benjamin.baudry@hotmail.com>
2018-04-19 15:12:27 +0200

    ajout slider
    ajout slider et modification css liées

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+var slideIndex=0
+showDivs(slideIndex)
+
+function plusDivs(n){
+  showDivs(slideIndex+=n)
+}
+
+function showDivs(n){
+  var x = document.getElementsByClassName("mySlider")
+  if (n > x.length){
+    slideIndex=1
+  }
+  if (n < 1){
+    slideIndex = x.length
+  }
+  for (i =0; i < x.length; i++){
+    x[i].style.display = "none"
+  }
+  x[slideIndex-1].style.display = "block"
+}
